a3d4ceb2d34bdfefb69f607a82afc807fc71ac6e
GuilhermeFaioli <guilhermefa12@gmail.com>
2021-07-30 10:56:49 -0300

    first commit

---
+++ typescript-training/05-inheritance/Circle.ts
@@ -0,0 +1,25 @@
+import { Shape } from "./Shape";
+
+export class Circle extends Shape {
+
+    private _radius: number;
+    
+
+    constructor(theX : number, theY : number, radius : number) {
+        super(theX, theY);
+        this._radius = radius;
+    }
+
+    public get radius_1(): number {
+        return this._radius;
+    }
+
+    public set radius_1(value: number) {
+        this._radius = value;
+    }
+
+    public getInfo() : string {
+        return super.getInfo() + `, radius = ${this._radius}`;
+    } 
+
+}
