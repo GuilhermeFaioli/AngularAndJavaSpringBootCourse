2e5e8510d951e0ff0a94b4dfcda62cf737f1036d
GuilhermeFaioli <guilhermefa12@gmail.com>
2021-08-06 17:40:47 -0300

    Product Master-Detail View

--- angular-training/ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-training/ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -11,7 +11,7 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductListComponent implements OnInit {
 
-  products: Product[] = [];
+  products!: Product[];
   currentCategoryId: number = 1;
   searchMode: boolean = false;
 
